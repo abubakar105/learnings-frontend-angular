dcd513b2477434aa1621dfd4a42aef949ce64158
AbuBakr <Abubakar.59132@gmail.com>
2025-05-23 16:12:33 +0500

    designed layout for the website home page

---
+++ src/app/Fearure/Inner/User/user-navbar/user-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-navbar.component.html',
+  styleUrls: ['./user-navbar.component.css']
+})
+export class UserNavbarComponent {
+
+}
--- src/app/Fearure/Inner/home/home.component.ts
+++ src/app/Fearure/Inner/home/home.component.ts
@@ -1,30 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { UserLayoutComponent } from "../User/user-layout/user-layout.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [UserLayoutComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    this.fetchUserData();
-  }
-
-  fetchUserData(): void {
-    this.http.get('https://localhost:7195/api/User')
-      .subscribe(
-        (response) => {
-          console.log('API Response:', response);
-        },
-        (error) => {
-          console.error('Error fetching user data:', error);
-        }
-      );
-  }
 }
