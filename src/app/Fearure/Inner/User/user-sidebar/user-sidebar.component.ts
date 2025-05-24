a7d33cceeb33084dd90695ae91928925bf2c2287
AbuBakr <Abubakar.59132@gmail.com>
2025-05-24 23:58:54 +0500

    added filter by price range  UI

--- src/app/Fearure/Inner/User/user-sidebar/user-sidebar.component.ts
+++ src/app/Fearure/Inner/User/user-sidebar/user-sidebar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SearchProdutcCategoryComponent } from "./search-produtc-category/search-produtc-category.component";
 import { SortByFilterComponent } from "./sort-by-filter/sort-by-filter.component";
 import { FilterByGenderComponent } from "./filter-by-gender/filter-by-gender.component";
+import { FilterByPriceComponent } from "./filter-by-price/filter-by-price.component";
 
 @Component({
   selector: 'app-user-sidebar',
   standalone: true,
-  imports: [SearchProdutcCategoryComponent, SortByFilterComponent, FilterByGenderComponent],
+  imports: [SearchProdutcCategoryComponent, SortByFilterComponent, FilterByGenderComponent, FilterByPriceComponent],
   templateUrl: './user-sidebar.component.html',
   styleUrls: ['./user-sidebar.component.css']
 })
