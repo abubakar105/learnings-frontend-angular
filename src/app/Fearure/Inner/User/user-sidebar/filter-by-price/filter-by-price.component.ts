c11bd40c1716c182184308d6b7681300db55bf15
AbuBakr <Abubakar.59132@gmail.com>
2025-05-24 23:57:46 +0500

    added filter by price range UI

---
+++ src/app/Fearure/Inner/User/user-sidebar/filter-by-price/filter-by-price.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-filter-by-price',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './filter-by-price.component.html',
+  styleUrls: ['./filter-by-price.component.css']
+})
+export class FilterByPriceComponent {
+  rangeMin: number = 0;
+  rangeMax: number = 100000;
+  minPrice: number = this.rangeMin;
+  maxPrice: number = this.rangeMax;
+  step: number = 100;
+
+  onMinInput(value: number) {
+    this.minPrice = Math.min(Math.max(this.rangeMin, value), this.maxPrice);
+  }
+  onMaxInput(value: number) {
+    this.maxPrice = Math.max(Math.min(this.rangeMax, value), this.minPrice);
+  }
+}
