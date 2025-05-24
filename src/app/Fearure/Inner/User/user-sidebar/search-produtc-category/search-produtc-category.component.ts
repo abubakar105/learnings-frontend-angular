72cf115ba48150ecefceadca68c4715ad972d2d5
AbuBakr <Abubakar.59132@gmail.com>
2025-05-24 23:16:09 +0500

    added a search category filter UI

---
+++ src/app/Fearure/Inner/User/user-sidebar/search-produtc-category/search-produtc-category.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductCategoryService } from '../../../../../Core/Services/ProductCategoryService';
+import { ToastService } from '../../../../../Core/Services/ToastService';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search-produtc-category',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './search-produtc-category.component.html',
+  styleUrls: ['./search-produtc-category.component.css'],
+})
+export class SearchProdutcCategoryComponent implements OnInit {
+  categoryList: any[] = [];
+  constructor(
+    private categorySerive: ProductCategoryService,
+    private toast: ToastService
+  ) {}
+  ngOnInit(): void {
+    this.categorySerive.getAllProductsParentCategory().subscribe(
+      (res) => {
+        if (res.status == 200) {
+          this.categoryList = res.data;
+        } else {
+          this.toast.error(res.message, 'Error');
+        }
+      },
+      (err) => {
+        this.toast.error('Unable to load categories', 'Error');
+      }
+    );
+  }
+  trackById(_: number, item: any) {
+  return item.attributeId;
+}
+}
--- src/app/Fearure/Inner/User/user-sidebar/user-sidebar.component.ts
+++ src/app/Fearure/Inner/User/user-sidebar/user-sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchProdutcCategoryComponent } from "./search-produtc-category/search-produtc-category.component";
 
 @Component({
   selector: 'app-user-sidebar',
   standalone: true,
-  imports: [],
+  imports: [SearchProdutcCategoryComponent],
   templateUrl: './user-sidebar.component.html',
   styleUrls: ['./user-sidebar.component.css']
 })
