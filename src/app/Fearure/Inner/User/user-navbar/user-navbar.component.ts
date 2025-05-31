a84ce52502418ed951b0048a22a3d3ffe6415db7
AbuBakr <Abubakar.59132@gmail.com>
2025-06-01 00:00:42 +0500

    added a UI for the user profile and update it

--- src/app/Fearure/Inner/User/user-navbar/user-navbar.component.ts
+++ src/app/Fearure/Inner/User/user-navbar/user-navbar.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-navbar.component.html',
   styleUrls: ['./user-navbar.component.css']
 })
 export class UserNavbarComponent {
+ dropdownOpen = false;
 
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  closeDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
 }
