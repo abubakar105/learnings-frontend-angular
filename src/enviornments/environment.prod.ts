ed0d91c93f103eeb76dfd9981d4d6d097e00b049
AbuBakr <Abubakar.59132@gmail.com>
2025-10-26 16:34:31 +0500

    added env

--- src/enviornments/environment.prod.ts
+++ src/enviornments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7195/api',
-    apiHUBUrl: 'https://localhost:7195',
+    apiUrl: 'http://learnings-api-prod.feg0dughchd3d3bj.centralus.azurecontainer.io:8080/api',
+    apiHUBUrl: 'http://learnings-api-prod.feg0dughchd3d3bj.centralus.azurecontainer.io:8080',
     hubsPath: '/hubs/reviews',
     cloudinary: {
     cloudName: 'dtlgcy0go',
-    uploadPreset: 'Ecommerece',
-    azureAd: {
-    clientId: '2771ca11-46c8-4052-9c47-29f551ed5e6c',
+    uploadPreset: 'Ecommerece'
+  },
+  azureAd: {
+    clientId: 'de9cc888-1e10-4879-b26a-3b10a4fbc6de',
     authority: 'https://login.microsoftonline.com/4a369bea-f988-4d2b-baae-0d98d5e87d25',
     redirectUri: 'http://localhost:4200/admin',
     postLogoutRedirectUri: 'http://localhost:4200',
@@ -16,9 +17,8 @@ export const environment = {
       'profile',
       'email',
       'offline_access',
-      'api://2771ca11-46c8-4052-9c47-29f551ed5e6c/Admin.Access'
+      'api://de9cc888-1e10-4879-b26a-3b10a4fbc6de/Admin.Access'
     ]
-  }
   }
   };
   
---
+++ src/enviornments/environment.staging.ts
@@ -0,0 +1,24 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://learnings-api-staging.d3a7hqf3hacfe4cm.centralus.azurecontainer.io:8080/api',
+  apiHUBUrl: 'http://learnings-api-staging.d3a7hqf3hacfe4cm.centralus.azurecontainer.io:8080',
+  hubsPath: '/hubs/reviews',
+  cloudinary: {
+    cloudName: 'dtlgcy0go',
+    uploadPreset: 'Ecommerece',
+  },
+  azureAd: {
+    clientId: '2771ca11-46c8-4052-9c47-29f551ed5e6c',
+    authority:
+      'https://login.microsoftonline.com/4a369bea-f988-4d2b-baae-0d98d5e87d25',
+    redirectUri: 'http://localhost:4200/admin',
+    postLogoutRedirectUri: 'http://localhost:4200',
+    scopes: [
+      'openid',
+      'profile',
+      'email',
+      'offline_access',
+      'api://2771ca11-46c8-4052-9c47-29f551ed5e6c/Admin.Access',
+    ],
+  },
+};
